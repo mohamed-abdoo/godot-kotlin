25ab07e313a785c88109670efe1fe4e04feb04a8
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-04-29 00:36:38 +0200

    Replace Klaxon by Jackson to deserialize API

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin", version = "1.3.71"))
-    implementation("com.beust:klaxon:3.0.1") //5.2
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
 
     //Remember to change version according to DependenciesVersion
     implementation("com.squareup:kotlinpoet:1.5.0")
